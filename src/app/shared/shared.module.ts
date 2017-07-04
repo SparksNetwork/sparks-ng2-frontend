f921d12cb73a122c9e7cc9485c56052a1216e24f
Emanuel Virca <emanuelv@imprezzio.com>
2017-07-04 11:36:39 +0300

    created shared module to be used across tests

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,17 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HeaderSimpleComponent } from './header-simple/header-simple.component';
 import { HeaderCarouselComponent } from './header-carousel/header-carousel.component';
 import { NavComponent } from './nav/nav.component';
+import { CardItemComponent } from './card-item/card-item.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent],
-  exports: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent]
+  declarations: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent],
+  exports: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent]
 })
 export class SharedModule { }
+
+export const sharedComponents = [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent];
+
