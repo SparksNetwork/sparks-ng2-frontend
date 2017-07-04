10858ff2b9cab7edfad9df37e400ac7e9ad85b06
Emanuel Virca <emanuelv@imprezzio.com>
2017-07-04 11:37:59 +0300

    fix import of RouterTesingModule and use shared components in app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,37 @@
+import { Component } from "@angular/core";
 import { TestBed, async } from '@angular/core/testing';
-import RouterTestingModule from '@angular/cli'
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule, Routes, provideRoutes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { sharedComponents } from "app/shared/shared.module";
+
+@Component({
+  selector: 'as-test-cmp',
+  template: '<div class="title">Hello test</div>'
+})
+class TestRouterComponent {
+}
+
+let config: Routes = [
+  {
+    path: '', component: TestRouterComponent
+  }
+];
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        TestRouterComponent,
+        AppComponent,
+        sharedComponents
       ],
       imports: [
+        RouterModule,
         RouterTestingModule
       ],
+      providers: [provideRoutes(config)]
     }).compileComponents();
   }));
 
@@ -19,18 +39,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
   }));
 });
