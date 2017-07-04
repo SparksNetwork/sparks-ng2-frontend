7323b43073fc54f57be885c170c4d6e56d9f807f
Emanuel Virca <emanuelv@imprezzio.com>
2017-07-04 15:35:20 +0300

    remove init code from events component

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({ 
-  template: `events <router-outlet></router-outlet>`,  
+  template: `<router-outlet></router-outlet>`,  
 })
 export class EventsComponent  {}
