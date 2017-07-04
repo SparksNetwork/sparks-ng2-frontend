b8e7fa9e626ebfb410f0fa2a0ad6fceed7fddd5e
Emanuel Virca <emanuelv@imprezzio.com>
2017-07-04 15:37:53 +0300

    event details - integrate card-item and schedule components

--- src/app/events/event/event.component.ts
+++ src/app/events/event/event.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CardItemType } from "app/shared/card-item/card-item.enum";
+import { IScheduleItem } from "app/shared/schedule/schedule-item.interface";
 
 @Component({
   selector: 'app-event',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventComponent implements OnInit {
 
+  cardItemType = CardItemType;
+  scheduleItems: IScheduleItem[];
+
   constructor() { }
-
+  
   ngOnInit() {
+    this.scheduleItems = [
+      { title: "Head Squad", description: null, date: "june 1st, 2017 - 2:00 PM", karmaPoints: null, shift: "Shift - 1" },
+      { title: "Security Crew", description: null, date: "june 2nd, 2017 - 2:00 PM", karmaPoints: null, shift: "Shift - 2" },
+      { title: "Head Squad", description: null, date: "june 1st, 2017 - 2:00 PM", karmaPoints: null, shift: "Shift - 3" },
+    ];
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,16 +6,17 @@ import { HeaderSimpleComponent } from './header-simple/header-simple.component';
 import { HeaderCarouselComponent } from './header-carousel/header-carousel.component';
 import { NavComponent } from './nav/nav.component';
 import { CardItemComponent } from './card-item/card-item.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent],
-  exports: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent]
+  declarations: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent, ScheduleComponent],
+  exports: [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent, ScheduleComponent]
 })
 export class SharedModule { }
 
-export const sharedComponents = [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent];
+export const sharedComponents = [PageNotFoundComponent, HeaderSimpleComponent, HeaderCarouselComponent, NavComponent, CardItemComponent, ScheduleComponent];
 
