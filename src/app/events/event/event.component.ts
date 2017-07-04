0b68fe8c0834d62b1c098f3e683edd0c16ed0732
Emanuel Virca <emanuelv@imprezzio.com>
2017-07-04 17:26:18 +0300

    add images as input array to header-carousel component

--- src/app/events/event/event.component.ts
+++ src/app/events/event/event.component.ts
@@ -8,17 +8,24 @@ import { IScheduleItem } from "app/shared/schedule/schedule-item.interface";
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
-
+  
   cardItemType = CardItemType;
   scheduleItems: IScheduleItem[];
+  images: string[];
 
   constructor() { }
-  
+
   ngOnInit() {
     this.scheduleItems = [
       { title: "Head Squad", description: null, date: "june 1st, 2017 - 2:00 PM", karmaPoints: null, shift: "Shift - 1" },
       { title: "Security Crew", description: null, date: "june 2nd, 2017 - 2:00 PM", karmaPoints: null, shift: "Shift - 2" },
       { title: "Head Squad", description: null, date: "june 1st, 2017 - 2:00 PM", karmaPoints: null, shift: "Shift - 3" },
     ];
+
+    this.images = [
+      'https://placeimg.com/1140/410/animals/grayscale',
+      'https://placeimg.com/1140/410/people/grayscale',
+      'https://placeimg.com/1140/410/nature/grayscale'
+    ]
   }
 }
