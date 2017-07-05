24e641dc57008ceb48f054ef071e28b20384f3db
Emanuel Virca <emanuelv@imprezzio.com>
2017-07-05 14:16:06 +0300

    created add-to-calendar component enabeling user to select witch king of calendar to save to

---
+++ src/app/shared/add-to-calendar/add-to-calendar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import FileSaver from 'file-saver';
+
+import { AddToCalendarService } from "./add-to-calendar.service";
+import { AddToCalendar } from "app/shared/add-to-calendar/add-to-calendar.model";
+
+@Component({
+  selector: 'app-add-to-calendar',
+  templateUrl: './add-to-calendar.component.html',
+  styleUrls: ['./add-to-calendar.component.css']
+})
+export class AddToCalendarComponent implements OnInit {
+
+  @Input() data: AddToCalendar;
+  googleUrl: string;
+  microsoftUrl: string;
+
+  constructor(private addToCalendarService: AddToCalendarService) { }
+
+  ngOnInit() {
+    this.googleUrl = this.addToCalendarService.getGoogleCalendarUrl(this.data);
+    this.microsoftUrl = this.addToCalendarService.getMicrosoftCalendarUrl(this.data);
+  }
+
+  downloadIcal($event) {
+    $event.preventDefault();
+
+    if (!this.data) return;
+
+    let fileName = this.addToCalendarService.getIcsFileName(this.data.title),
+      icsData = this.addToCalendarService.getIcsCalendar(this.data),
+      icsBlob = this.addToCalendarService.getIcsBlob(icsData);
+
+    FileSaver.saveAs(icsBlob, fileName);
+  }
+}
---
+++ src/app/shared/add-to-calendar/add-to-calendar.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from "@angular/core";
+import * as moment from 'moment';
+
+import { AddToCalendar } from "app/shared/add-to-calendar/add-to-calendar.model";
+import { DateService } from 'app/core/date.service';
+
+@Injectable()
+export class AddToCalendarService {
+
+    constructor(private dateService: DateService) { }
+
+    getMicrosoftCalendarUrl(data: AddToCalendar) {
+        if(!data) return;
+
+        var microsoftCalendarUrl = 'http://calendar.live.com/calendar/calendar.aspx?rru=addevent';
+        microsoftCalendarUrl += '&summary=' + encodeURIComponent(data.title);
+        microsoftCalendarUrl += '&dtstart=' + encodeURIComponent(data.startDate) + '&dtend=' + encodeURIComponent(data.endDate);
+        microsoftCalendarUrl += '&description=' + encodeURIComponent(data.description);
+        microsoftCalendarUrl += '&location=' + encodeURIComponent(data.location);
+
+        return microsoftCalendarUrl;
+    }
+
+    getGoogleCalendarUrl(data: AddToCalendar) {
+        if(!data) return;
+
+        var googleCalendarUrl = 'https://www.google.com/calendar/render?action=TEMPLATE';
+        googleCalendarUrl += '&text=' + encodeURIComponent(data.title);
+        googleCalendarUrl += '&dates=' + encodeURIComponent(data.startDate) + '/' + encodeURIComponent(data.endDate);
+        googleCalendarUrl += '&details=' + encodeURIComponent(data.description);
+        googleCalendarUrl += '&location=' + encodeURIComponent(data.location);
+
+        return googleCalendarUrl;
+    }
+
+    getIcsCalendar(data: AddToCalendar) {
+        if(!data) return;
+        return [
+            'BEGIN:VCALENDAR',
+            'VERSION:2.0',
+            'BEGIN:VEVENT',
+            'CLASS:PUBLIC',
+            'DESCRIPTION:' + this.formatIcsText(data.description),
+            'DTSTART:' + this.dateService.toUniversalTime(data.startDate),
+            'DTEND:' + this.dateService.toUniversalTime(data.endDate),
+            'LOCATION:' + this.formatIcsText(data.location),
+            'SUMMARY:' + this.formatIcsText(data.title),
+            'TRANSP:TRANSPARENT',
+            'END:VEVENT',
+            'END:VCALENDAR',
+            'UID:' + this.getUid(),
+            'DTSTAMP:' + this.getTimeCreated(),
+            'PRODID:angular-addtocalendar'
+        ].join('\n');
+    }
+
+    /**
+     * Removes line breaks and ensures that the string is no
+     * longer than maxLength chars (or 75 chars if none specified).
+     *
+     * @param  {String} s         string to sanitize
+     * @param  {Number} maxLength index of string to truncate at
+     * @return {String}
+     */
+    private formatIcsText(str: string, maxLength?: number) {
+        if (!str) {
+            return '';
+        }
+        str = str.replace(/\n/g, '\\n');
+
+        if (maxLength)
+            str = str.substring(0, maxLength);
+
+        return str;
+    }
+
+    /**
+      * Returns a random base 36 hash for iCal UID.
+      * 
+      * @return {String}
+      */
+    private getUid() {
+        return Math.random().toString(36).substr(2);
+    }
+
+    /**
+     * Returns a universal timestamp of current time.
+     * 
+     * @return {String}
+     */
+    private getTimeCreated() {
+        return moment().format('YYYYMMDDTHHmmss');
+    }
+
+    /**
+    * The name of the file will be the event title with alphanumeric chars
+    * having the extension `.ics`.
+    *
+    * @param  {String} icsData
+    * @return {Blob}
+    */
+    getIcsBlob(icsData) {
+        return new Blob([icsData], {
+            type: 'application/octet-stream'
+        });
+    }
+
+    /**
+    * Transforms given string to be valid file name.
+    * 
+    * @param  {String} title
+    * @return {String}
+    */
+    getIcsFileName(title: string) {
+        if (!title) {
+            return 'event.ics';
+        }
+        return title.replace(/[^\w ]+/g, '') + '.ics';
+    }
+}
