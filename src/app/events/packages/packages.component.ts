4bf8325c7ec1de4ef19678d38950bc4fed2dc219
IPZ\monicab <imprezzio-monicai@bitbucket.org>
2017-07-07 15:54:57 +0300

    UI Static HTML -  Opportunity Details - finished basic structure
    SparksNetw-108520  [UI Static] - Opportunity Details

--- src/app/events/packages/packages.component.ts
+++ src/app/events/packages/packages.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({ 
-  template: `packages dropdown <router-outlet></router-outlet>`,  
+  template: `<router-outlet></router-outlet>`,  
 })
 export class PackagesComponent  {}
