7c3bb64362360b702d439d2ab3987590c5ce43dd
ildikod <ildikod@imprezzio.com>
2017-07-19 01:23:21 +0300

    renamed event-card component to dashboard-projects

--- src/app/dashboard/dashboard-projects/dashboard-projects.component.spec.ts
+++ src/app/dashboard/dashboard-projects/dashboard-projects.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EventCardComponent } from './event-card.component';
+import { DashboardProjectsComponent } from './dashboard-projects.component';
 
-describe('EventCardComponent', () => {
-  let component: EventCardComponent;
-  let fixture: ComponentFixture<EventCardComponent>;
+describe('DashboardProjectsComponent', () => {
+  let component: DashboardProjectsComponent;
+  let fixture: ComponentFixture<DashboardProjectsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EventCardComponent ]
+      declarations: [ DashboardProjectsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EventCardComponent);
+    fixture = TestBed.createComponent(DashboardProjectsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/dashboard/dashboard-projects/dashboard-projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-projects',
+  templateUrl: './dashboard-projects.component.html',
+  styleUrls: ['./dashboard-projects.component.css']
+})
+export class DashboardProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
