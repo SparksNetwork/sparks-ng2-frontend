dcc353901c0d687098eb9da21a6c065cc346a759
ildikod <ildikod@imprezzio.com>
2017-08-07 17:43:33 +0300

    Added cancel-volunteering and cancel-confirmation components

---
+++ src/app/projects/cancel-confirmation/cancel-confirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-confirmation',
+  templateUrl: './cancel-confirmation.component.html',
+  styleUrls: ['./cancel-confirmation.component.scss']
+})
+export class CancelConfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
