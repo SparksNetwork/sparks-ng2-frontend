77df578cb56a2405013eb589f4b0e5edc682777e
ildikod <ildikod@imprezzio.com>
2017-08-10 11:38:20 +0300

    no message

--- src/app/user-profile/user-profile-routing.module.ts
+++ src/app/user-profile/user-profile-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserProfileComponent } from "app/user-profile/user-profile.component";
-import { ProfileActivityDetailsComponent } from "app/user-profile/profile-activity-details/profile-activity-details.component";
-import { PublicProfileComponent } from "app/user-profile/public-profile/public-profile.component";
-import { BadgesComponent } from "app/user-profile/badges/badges.component";
-import { TrophiesComponent } from "app/user-profile/trophies/trophies.component";
-import { AccoladesComponent } from "app/user-profile/accolades/accolades.component";
-import { ProfileInfoComponent } from "app/user-profile/profile-info/profile-info.component";
-import { EditProfileDetailsComponent } from "app/user-profile/edit-profile-details/edit-profile-details.component";
-import { EditContactInfoComponent } from "app/user-profile/edit-contact-info/edit-contact-info.component";
-import { ChangePasswordComponent } from "app/user-profile/change-password/change-password.component";
-import { ConfirmChangesComponent } from "app/user-profile/confirm-changes/confirm-changes.component";
-import { CongratsTrophyComponent } from "app/user-profile/trophies/congrats-trophy/congrats-trophy.component";
-import { CongratsAccoladesComponent } from "app/user-profile/accolades/congrats-accolades/congrats-accolades.component";
-import { SelectBadgeComponent } from "app/user-profile/badges/select-badge/select-badge.component";
-import { ConfirmBadgeComponent } from "app/user-profile/badges/confirm-badge/confirm-badge.component";
-import { CongratsBadgeComponent } from "app/user-profile/badges/congrats-badge/congrats-badge.component";
+import { UserProfileComponent } from 'app/user-profile/user-profile.component';
+import { ProfileActivityDetailsComponent } from 'app/user-profile/profile-activity-details/profile-activity-details.component';
+import { PublicProfileComponent } from 'app/user-profile/public-profile/public-profile.component';
+import { BadgesComponent } from 'app/user-profile/badges/badges.component';
+import { TrophiesComponent } from 'app/user-profile/trophies/trophies.component';
+import { AccoladesComponent } from 'app/user-profile/accolades/accolades.component';
+import { ProfileInfoComponent } from 'app/user-profile/profile-info/profile-info.component';
+import { EditProfileDetailsComponent } from 'app/user-profile/edit-profile-details/edit-profile-details.component';
+import { EditContactInfoComponent } from 'app/user-profile/edit-contact-info/edit-contact-info.component';
+import { ChangePasswordComponent } from 'app/user-profile/change-password/change-password.component';
+import { ConfirmChangesComponent } from 'app/user-profile/confirm-changes/confirm-changes.component';
+import { CongratsTrophyComponent } from 'app/user-profile/trophies/congrats-trophy/congrats-trophy.component';
+import { CongratsAccoladesComponent } from 'app/user-profile/accolades/congrats-accolades/congrats-accolades.component';
+import { SelectBadgeComponent } from 'app/user-profile/badges/select-badge/select-badge.component';
+import { ConfirmBadgeComponent } from 'app/user-profile/badges/confirm-badge/confirm-badge.component';
+import { CongratsBadgeComponent } from 'app/user-profile/badges/congrats-badge/congrats-badge.component';
+import { QuestsComponent } from 'app/user-profile/quests/quests.component';
 
 const routes: Routes = [
   {
@@ -82,6 +83,10 @@ const routes: Routes = [
         path: 'confirm-changes',
         component: ConfirmChangesComponent
       },
+      {
+        path: 'quests',
+        component: QuestsComponent
+      }
     ]
   }
 ];
